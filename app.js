7f4dd9e9663d0087e45f8af584d6c4a42d70fa0e
gogiranyan <91252747+gogiranyan@users.noreply.github.com>
2022-04-17 01:25:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const { json } = require('express/lib/response');
 const app = express()
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
+const mysql = require('mysql');
+const { Socket } = require('dgram');
+
+//connect mysql---------------
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "bses10302"
+});
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Server is Connected!");
+});
+
+
+
 
 const wss = new WebSocket.Server({ server:server });
 
@@ -12,12 +28,17 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-
     // wss.clients.forEach(function each(client) {
     //   if (client !== ws && client.readyState === WebSocket.OPEN) {
     //     client.send(message);
     //   }
     // });
+    for(const client of wss.clients)
+{
+    console.log('================================================');
+    console.log(client);
+//   client.close();
+}
     
   });
 
@@ -30,3 +51,6 @@ wss.on('connection', function connection(ws) {
 app.get('/', (req, res) => res.send('Hello World!'))
 
 server.listen(3000, () => console.log(`Lisening on port :3000`))
+
+
+Object
