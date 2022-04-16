7fca8a6d6968e9315a7a53858f45136ec0036980
gogiranyan <91252747+gogiranyan@users.noreply.github.com>
2022-04-16 21:30:49 +0800

    success_v1

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express')
-const app = express();
-const server =require('http').createServer(app);
-const WebSocket = require('ws')
-const wss = new WebSocket.Server({server:server});
+const app = express()
+const server = require('http').createServer(app);
+const WebSocket = require('ws');
 
-wss.on('connection',function connection(ws){
-    console.log('client Connected!');
+const wss = new WebSocket.Server({ server:server });
 
-    ws.send('Welcome Clinet');
+wss.on('connection', function connection(ws) {
+  console.log('A new client Connected!');
+  ws.send('Welcome New Client!');
 
-    ws.on('message', function incoming(message){
-        console.log('received: %s', message);
-        ws.send('Got ur msg its: '+ message);
-    })
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
 
-})
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+    
+  });
+});
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+server.listen(3000, () => console.log(`Lisening on port :3000`))
