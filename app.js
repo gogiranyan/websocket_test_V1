72664cdf8df153ea202dcc1c62b8244f829dcfb6
gogiranyan <91252747+gogiranyan@users.noreply.github.com>
2022-04-17 15:34:16 +0800

    11%

--- app.js
+++ app.js
@@ -25,27 +25,39 @@ wss.on('connection', function connection(ws) {
   ws.send('Welcome New Client!');
 
   ws.on('message', function incoming(message) {
-
   // console.log('received: %s', message);
-    console.log(JSON.parse(message));
-    // wss.clients.forEach(function each(client) {
-    //   if (client !== ws && client.readyState === WebSocket.OPEN) {
-    //     client.send(message);
-    //   }
-    // });
+    let obj = JSON.parse(message);
+    if(obj.device === 'machine' && obj.info == ture){
+      let machine
+    }else if(obj.device === 'phone'){
+      
+    }
 
-    
+
+    console.log(obj);
+
+
   });
 
 
-  ws.onclose = () => {
+  ws.onclose = (event) => {
     console.log('close connection');
+    
+    var code = event.code;
+  var reason = event.reason;
+  var wasClean = event.wasClean;
+  console.log(code);
+    console.log(reason);
+    console.log(wasClean)
   }
+  ws.on('closed', function incoming(message) {
+      console.log(message);
+  
+    });
+
 });
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 server.listen(3000, () => console.log(`Lisening on port :3000`))
 
-
-Object
