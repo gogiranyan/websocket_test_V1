14a2176e48b1fd9b6a404549062f20839da8bb56
gogiranyan <91252747+gogiranyan@users.noreply.github.com>
2022-05-16 21:38:34 +0800

    33

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ wss.on('connection', function connection(ws) {
     game_start(obj,ws,wss)
     game_info_to_machine(obj,wss,ws)
     machin_info_to_server(obj,ws,wss)
+    test_sql()
 
 
     let clients = wss.clients  //取得所有連接中的 client
@@ -294,8 +295,6 @@ function game_info_to_machine(obj,wss,ws){
   }
 }
 
-
-
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
@@ -305,17 +304,18 @@ function shuffle(array) {
 
 function machin_info_to_server(obj,ws,wss){
     if(obj.machin_info_to_server == true){
-    let data={
-      device: obj.device,
-      subject: obj.subject,
-      is_right:obj.right,
-      en_result:obj.en_result,
-      unix_time:obj.unix_time,
-      play_model:obj.play_model,
-      play_input:obj.play_input,
-      play_output:obj.play_output,
-      device_round:obj.device_round
-    }
+      if(obj.play_model == 0){
+        var count = 0
+        CLIENTS.forEach(e => {
+          if(e.device_round == game_round){
+            count+=1;
+          }
+        });
+        if(CLIENTS.length == count){
+
+        }
+      }
+
     
     let sql ="INSERT INTO history (device, subject, is_right, en_result, unix_time, play_output, play_input, play_model,device_round) VALUES ('"+obj.device+"','"+ obj.subject+"','"+ obj.is_right+"','"+ obj.en_result+"','"+ obj.unix_time+"','"+ obj.play_output+"','"+ obj.play_input+"','"+ obj.play_model+"','"+ obj.device_round+"')";
     con.query(sql,function(err,result){
@@ -337,6 +337,37 @@ function get_history(obj,ws,wss){
   if(obj.get_history == true){
     let sql = "SELECT * FROM playing_list";
   }
+}
+function test_sql(){
+  // // let sql= "SELECT * FROM `subject` FULL JOIN `history`"
+  // let sql=  "SELECT subject, account FROM playing_list, access WHERE playing_list.id = 1";
+  // console.log(sql)
+
+  // con.query(sql,function(err,result){
+  //   if(err) throw err; 
+  //   console.log("result: "+JSON.stringify(result))
+  // })
+  let sql = "SELECT * FROM playing_list WHERE id = 1";
+  con.query(sql,function(err,presult){
+    let temp = JSON.stringify(presult)
+    let p_list = JSON.parse(temp)
+    console.log("p_list~: "+ temp )
+    if(err) throw err;
+    let sql = "SELECT * FROM subject";
+    con.query(sql,function(err,result){
+      if(err) throw err;
+      console.log("presult,id :"+ presult.id)
+    })
+  })
+}
+function server_to_machine(){
+  // let sql = "SELECT * FROM playing_list WHERE id = 1";
+  // con.query(sql,function(err,p_list){
+  //   let p_list = JSON.stringify(p_list)
+  //   if(err) throw err;
+  //   let sql = ""
+  // })
+
 }
 
  
